95dfa5970c677b86cd71a5929a5151a71a6a8149
adarshmoger <adarshmoger2000@gmail.com>
2025-10-27 14:34:14 +0530

    Updated data source block

--- main.tf
+++ main.tf
@@ -20,15 +20,14 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "acr_rg" {
+data "azurerm_resource_group" "acr_rg" {
   name     = var.resource_group_name
-  location = var.location
 }
 
 module "container_registry" {
   source = "./modules/container_registry"
-  resource_group_name = azurerm_resource_group.acr_rg.name
-  location            = azurerm_resource_group.acr_rg.location
+  resource_group_name = data.azurerm_resource_group.acr_rg.name
+  location            = data.azurerm_resource_group.acr_rg.location
   container_registry_name = var.container_registry_name
   georeplication_location = var.georeplication_location
 
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,4 +1,3 @@
-container_registry_name   = "PengwinACR"
+container_registry_name   = "SharkACR"
 georeplication_location   = "Japan East"
 resource_group_name = "Pengwin-rg-acr"
-location            = "Japan West"
